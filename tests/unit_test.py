f4f1de027bd8697bf2d23d78fb2e334098f7f296
antokish <okish04@gmail.com>
2018-05-31 10:06:32 +0300

    Chore #157990868 changed url in test to match recommended pattern

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -13,13 +13,26 @@ import json
 import os
 import pytest
 
-@pytest.mark.usefixtures("requests")
+from resources.User_request import dtrequest
+from run import create_app
+import config
+
+
+@pytest.mark.unittest
 class ApiTest(unittest.TestCase):
     """ API endpoints test case"""
 
+    def setUp(self):
+        """Declare test variables and initialize app."""
+        self.app = create_app(config_filename="testing")
+        self.client = self.app.test_client
+        self.status = {'status': 'Active'}
+
+        
+
     def test_api_can_get_all_requests(self):
         """Test api Get all the requests for a logged in user"""
-        res = self.client().post('/api/v1/user/requests/', requests)
+        res = self.client().post('/api/v1/user/requests/', dtrequest)
         self.assertEqual(res.status_code, 201)
         res = self.client().get('/api/v1/user/requests')
         self.assertEqual(res.status_code, 200)
@@ -27,26 +40,26 @@ class ApiTest(unittest.TestCase):
 
     def test_api_can_get_request_by_id(self):
         """Test api can get a request for a logged in user"""
-        res = self.client().post('/api/v1/user/requests/', requests)
+        res = self.client().post('/api/v1/user/requests/', dtrequest)
         self.assertEqual(res.status_code, 201)
-        json_res = json.loads()
+        json_res = json.loads(dtrequest)
         result = self.client().get(
-            '/api/v1/user/requests{}'.format(json_res['id']))
+            '/api/v1/user/requests{}'.format(json_res['status']))
         self.assertEqual(result.status_code, 200)
-        self.assertIn("John doe")
+        self.assertIn("Resolved", dtrequest)
 
     def test_api_request_can_be_modified(self):
         """Test api can modify a request"""
-        res = self.client().post('/api/v1/user/requests/', data={"user":"john doe"})
-        res = self.client().put('/api/v1/user/requests/1', data ={"user":"John sue doe"})
+        res = self.client().post('/api/v1/user/requests/1', data={"status":"Approved"})
+        res = self.client().put('/api/v1/user/requests/1', data ={"user":"Resolved"})
         self.assertEqual(res.status_code, 200)
         results = self.client().get('/api/v1/user/requests/1')
-        self.assertIn('sue', str(requests))
+        self.assertIn('resolved', str(results.object))
 
 
     def test_api_can_create_request(self):
         """Test api can create a request"""
-        res = self.client().post('/api/v1/user/requests/', data=requests)
+        res = self.client().post('/api/v1/user/requests/', data=dtrequest)
         self.assertEqual(res.status_code, 201)
         self.assertIn('John doe', str(res.data))
 
